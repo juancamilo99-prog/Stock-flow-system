598a998d2359faa6a82c3359873544dbe9833a1b
monterocamilo99@hotmail.com <monterocamilo99@hotmail.com>
2026-08-08 18:10:02 +0200

    feat(auditoria) implement audit loggin for location creation and updates

--- backend/src/main/java/org/jcdev/stockflow/backend/service/UbicacionService.java
+++ backend/src/main/java/org/jcdev/stockflow/backend/service/UbicacionService.java
@@ -1,10 +1,11 @@
 package org.jcdev.stockflow.backend.service;
 
+import jakarta.transaction.Transactional;
 import org.jcdev.stockflow.backend.dto.actualizardto.ActualizarUbicacionDto;
 import org.jcdev.stockflow.backend.dto.creardto.CrearUbicacionDto;
-import org.jcdev.stockflow.backend.entity.Categoria;
-import org.jcdev.stockflow.backend.entity.Producto;
-import org.jcdev.stockflow.backend.entity.Ubicacion;
+import org.jcdev.stockflow.backend.entity.*;
+import org.jcdev.stockflow.backend.enums.auditoria.EntidadAuditoria;
+import org.jcdev.stockflow.backend.enums.auditoria.TipoAccion;
 import org.jcdev.stockflow.backend.repository.ProductoRepository;
 import org.jcdev.stockflow.backend.repository.UbicacionRepository;
 import org.springframework.stereotype.Service;
@@ -16,10 +17,12 @@ public class UbicacionService {
 
     private final UbicacionRepository ubicacionRepository;
     private final ProductoRepository productoRepository;
+    private final AuditoriaService auditoriaService;
 
-    public UbicacionService(UbicacionRepository ubicacionRepository, ProductoRepository productoRepository) {
+    public UbicacionService(UbicacionRepository ubicacionRepository, ProductoRepository productoRepository, AuditoriaService auditoriaService) {
         this.ubicacionRepository = ubicacionRepository;
         this.productoRepository = productoRepository;
+        this.auditoriaService = auditoriaService;
     }
 
     //obtener todas las ubicaciones
@@ -37,6 +40,7 @@ public class UbicacionService {
     }
 
     //crear una ubicacion
+    @Transactional
     public Ubicacion crearUbicacion(CrearUbicacionDto crearUbicacionDto){
         String codigo = crearUbicacionDto.getCodigo().trim().toUpperCase();
         String descripcion = crearUbicacionDto.getDescripcion().trim();
@@ -46,13 +50,26 @@ public class UbicacionService {
         Ubicacion ubicacion = new Ubicacion();
         ubicacion.setCodigo(codigo);
         ubicacion.setDescripcion(descripcion);
-        return ubicacionRepository.save(ubicacion);
+        ubicacion = ubicacionRepository.save(ubicacion);
+
+        //TODO el usuario se obtendra del SecurityContext
+        //TODO Volver a hacer obligatorio id_usuario en la base de datos (NOT NULL) una vez todas las auditorías tengan un actor real.
+        auditoriaService.registrarAuditoria(
+                TipoAccion.CREAR,
+                EntidadAuditoria.UBICACION,
+                "Se ha creado una nueva ubicacion: "+ubicacion.getId(),
+                ubicacion.getId(),
+                null
+        );
+        return ubicacion;
     }
 
     //actualizar una ubicacion
+    @Transactional
     public Ubicacion actualizarUbicacion(Long idUbicacion,ActualizarUbicacionDto actualizarUbicacionDto){
         Ubicacion ubicacion = ubicacionRepository.findById(idUbicacion)
                 .orElseThrow(() -> new IllegalArgumentException("La ubicacion no existe"+idUbicacion));
+        boolean detectarCambio = false;
         if (actualizarUbicacionDto.getDescripcion() != null) {
             String descripcionNueva = actualizarUbicacionDto.getDescripcion().trim();
             if (descripcionNueva.isBlank()){
@@ -62,6 +79,7 @@ public class UbicacionService {
                 throw new IllegalArgumentException("La nueva descripcion debe ser diferente de la actual");
             }
             ubicacion.setDescripcion(descripcionNueva);
+            detectarCambio = true;
         }
 
         if (actualizarUbicacionDto.getCodigo() != null) {
@@ -76,7 +94,21 @@ public class UbicacionService {
                 throw new IllegalArgumentException("La ubicacion ya existe: "+codigoNuevo);
             }
             ubicacion.setCodigo(codigoNuevo);
+            detectarCambio = true;
         }
-        return ubicacionRepository.save(ubicacion);
+
+        if (detectarCambio) {
+            ubicacion = ubicacionRepository.save(ubicacion);
+            auditoriaService.registrarAuditoria(
+                    TipoAccion.ACTUALIZAR,
+                    EntidadAuditoria.UBICACION,
+                    "Se ha actualizado una ubicacion: "+ubicacion.getId(),
+                    ubicacion.getId(),
+                    null
+            );
+        }else {
+            throw new IllegalStateException("No se detecto ningun cambio");
+        }
+        return ubicacion;
     }
 }
