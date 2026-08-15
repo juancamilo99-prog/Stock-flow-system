f301509a6debd4a3fb7718fbc4ee4cbf8b2cdf33
monterocamilo99@hotmail.com <monterocamilo99@hotmail.com>
2026-08-15 21:21:32 +0200

    feat(security) configure DaoAutheticationProvider

--- backend/src/main/java/org/jcdev/stockflow/backend/config/SecurityConfig.java
+++ backend/src/main/java/org/jcdev/stockflow/backend/config/SecurityConfig.java
@@ -2,7 +2,10 @@ package org.jcdev.stockflow.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -11,5 +14,17 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+
+        //Creamos el proveedor de autenticacion y le indicamos
+        //el servicio encargado de buscar los usuarios
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+        //Le indicamos el PasswordEncoder que utilizara para comparar
+        // la contraseña introducida con el hash guardado en la BD
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
     }
 }
