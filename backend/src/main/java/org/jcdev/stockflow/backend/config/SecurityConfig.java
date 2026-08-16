9edb3ee3625689d59d9174a8bfab1c905f9ad114
monterocamilo99@hotmail.com <monterocamilo99@hotmail.com>
2026-08-16 14:59:31 +0200

    feat(security) implement user authentication flow

--- backend/src/main/java/org/jcdev/stockflow/backend/config/SecurityConfig.java
+++ backend/src/main/java/org/jcdev/stockflow/backend/config/SecurityConfig.java
@@ -2,12 +2,16 @@ package org.jcdev.stockflow.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -32,5 +36,16 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration){
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.requestMatchers("/auth/login").permitAll()
+                                .requestMatchers(HttpMethod.POST,"/usuarios").permitAll()
+                                .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
     }
 }
